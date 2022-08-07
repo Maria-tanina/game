a639918b0bc9cc55867c62e0e566ad83a10bdffe
Maria-tanina <mariyatkach2002@gmail.com>
2022-08-07 16:50:44 +0300

    added bombs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var startBtn = document.querySelector('.start__btn');
 var gameBlock = document.querySelector('#game');
-var startBlock =  document.querySelector('#start');
+var startBlock = document.querySelector('#start');
 const player = document.getElementById('player');
 let countLifes = 3; // счетчик жизней
 var items = document.querySelector('.items');
@@ -11,6 +11,7 @@ let restartBtn = document.querySelector('.restart__btn');
 let audioplayer = document.querySelector('audio');
 let source = document.querySelector('audio source');
 let soundBtn = document.querySelector('#sound img');
+let boom = document.querySelector('.boom img');
 
 
 var sound = 'off';
@@ -32,7 +33,7 @@ function random(min, max) {
     return Math.floor(rand);
 };
 
-startBtn.onclick = function() {
+startBtn.onclick = function () {
     start();
 };
 
@@ -40,87 +41,107 @@ function start() {
     startBlock.style.display = 'none';
     game.style.display = 'block';
     createLifes();
-    
-    createItem(1, 7, 200)   // кидаем предметы
+
+    createItem(1, 7, 200); // кидаем предметы
 };
 
 // функция создания жизней игрока
 function createLifes() {
     let LifesBlock = document.querySelector("#lifes");
-        LifesBlock.innerHTML = "";      // очистка жизней
-    let count = 0;                 
-    while (count < countLifes) {        // создание жизней по циклу 
+    LifesBlock.innerHTML = ""; // очистка жизней
+    let count = 0;
+    while (count < countLifes) { // создание жизней по циклу 
 
         let span = document.createElement("span");
         LifesBlock.appendChild(span);
         count++;
-    }    
+    }
 };
 
 /* функция смерти игрока */
 function die() {
     countLifes--;
 
-    if (countLifes <= 0)            // если жизни кончились
-    {   
+    if (countLifes <= 0) // если жизни кончились
+    {
         gameBlock.style.display = 'none';
         endGame.style.display = 'block';
         scoreEnd.innerHTML = scorePoints;
-      //   EndGame();
+        //   EndGame();
     }
 }
-restartBtn.onclick = function() {
+restartBtn.onclick = function () {
     location.reload();
 };
 
 /* выбор скина для персонажа */
 document.querySelectorAll("#select-player span img").forEach(
-    el => el.onclick = function(e) {
+    el => el.onclick = function (e) {
         let selected = document.querySelector('.selected');
         selected.className = "";
         this.parentElement.className = "selected";
 
-        if(this.parentElement.className == "selected") {
+        if (this.parentElement.className == "selected") {
             player.className = this.parentElement.id;
         }
     }
 );
 
 /* движуха персонажа */
-document.onkeydown = function(event) {
-    if(event.code == "KeyA" && player.offsetLeft > 20) {
-        player.style.left = player.offsetLeft - 35 +"px";
-    } else if(event.code == "KeyD" && player.offsetLeft < event.target.clientWidth - 218) {
-        player.style.left = player.offsetLeft + 35 +"px";
+document.onkeydown = function (event) {
+    if (event.code == "KeyA" && player.offsetLeft > 20) {
+        player.style.left = player.offsetLeft - 35 + "px";
+    } else if (event.code == "KeyD" && player.offsetLeft < event.target.clientWidth - 218) {
+        player.style.left = player.offsetLeft + 35 + "px";
     }
 };
 
 /* создание предмета */
+
 function createItem(min, max, time) {
     items.innerHTML = "";
     let rand = random(min, max);
     let item = document.createElement("img");
-    item.setAttribute("src", "images/items/item_"+ rand +".png");
+    item.setAttribute("src", "images/items/item_" + rand + ".png");
+    console.log(rand);
+    if (rand == 8 || rand == 9) {
+        item.setAttribute('class', 'bomb');
+        console.dir(item);
+    }
     items.appendChild(item);
-    
+
     item.style.top = "-80px";
-    item.style.left = random(214, window.innerWidth - 115) +"px";
+    item.style.left = random(214, window.innerWidth - 115) + "px";
     let scorePoints = 0;
 
     fallItem(item, time);
+}
+// создание boom 
+function createBoom(top, left) {
+    var boom = document.createElement('div');
+    boom.className = 'boom';
+    boom.style.top = top + 'px';
+    boom.style.left = left + 'px';
+    gameBlock.appendChild(boom);
+    setTimeout(function () {
+        boom.remove();
+    }, 700);
 }
 
 /* падение предмета. */
 function fallItem(item, time) {
     let intID = setInterval(() => {
-        item.style.top = item.offsetTop + 15 +'px';
-        
-        if(item.offsetTop > window.outerHeight) {       // если не впоймал
+        item.style.top = item.offsetTop + 15 + 'px';
+
+        if (item.offsetTop > window.outerHeight) { // если не впоймал
             item.remove();
             clearInterval(intID);
-            die();
-            createLifes();
-            createItem(1, 7, 200);        // кидаем предметы снова
+            if (item.className !== 'bomb') {
+                die();
+                createLifes();
+            }
+            createItem(1, 9, 200); // кидаем предметы снова
+
         }
         cacheItem(item);
     }, time);
@@ -128,27 +149,32 @@ function fallItem(item, time) {
 
 /* ловим предмет */
 function cacheItem(item) {
-    if(player.className == "skin_1" &&                                      // если скин 1
-       player.offsetTop + 180 < item.offsetTop + (item.height / 2) &&
-       item.offsetTop + (item.height / 2) < player.offsetTop + 195)
-    {
-        if(player.offsetLeft + 52 < item.offsetLeft +  (item.width / 2) &&
-           item.offsetLeft +  (item.width / 2) < player.offsetLeft + 152) {
-            
+    if (player.className == "skin_1" && // если скин 1
+        player.offsetTop + 180 < item.offsetTop + (item.height / 2) &&
+        item.offsetTop + (item.height / 2) < player.offsetTop + 195) {
+
+        if (player.offsetLeft + 52 < item.offsetLeft + (item.width / 2) &&
+            item.offsetLeft + (item.width / 2) < player.offsetLeft + 152 && item.className == '') {
             score.lastElementChild.innerText = Number(scorePoints) + 1;
-            createItem(1, 7, 200)
-        }  
-    } else if(player.className == "skin_2" &&                               // если скин 2
-              player.offsetTop + 124 < item.offsetTop + (item.height / 2) &&
-              item.offsetTop + (item.height / 2) < player.offsetTop + 190)
-    {                
-                if(player.offsetLeft + 115 < item.offsetLeft + (item.width / 2) &&
-                   item.offsetLeft + (item.width / 2) < player.offsetLeft + 213) {
-                    
-                    score.lastElementChild.innerText = Number(scorePoints) + 1;
-                    createItem(1, 7, 200)
+            createItem(1, 9, 200);
+        } else if (player.offsetLeft + 52 < item.offsetLeft + (item.width / 2) && //если ловим бомбу
+            item.offsetLeft + (item.width / 2) < player.offsetLeft + 152 && item.className == 'bomb') {
+            createBoom(item.offsetTop - 100, item.offsetLeft - 50);
+            die();
+            createLifes();
+            createItem(1, 9, 200);
+
+        }
+
+    } else if (player.className == "skin_2" && // если скин 2
+        player.offsetTop + 124 < item.offsetTop + (item.height / 2) &&
+        item.offsetTop + (item.height / 2) < player.offsetTop + 190) {
+        if (player.offsetLeft + 115 < item.offsetLeft + (item.width / 2) &&
+            item.offsetLeft + (item.width / 2) < player.offsetLeft + 213) {
+
+            score.lastElementChild.innerText = Number(scorePoints) + 1;
+            createItem(1, 9, 200);
         }
     }
     scorePoints = score.lastElementChild.innerText;
 }
-
